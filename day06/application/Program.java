55c698890e46c5308ae833a61fb54e95505d959a
Alexandre Nascimento <Alexandrenas2003@gmail.com>
2023-12-26 20:05:18 -0300

    day06

---
+++ day06/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+        System.out.println("Dados do produto");
+        System.out.print("Nome: ");
+        product.name = sc.nextLine();
+        System.out.print("Preço: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantidade: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println(product.totalValue());
+        
+        System.out.printf("Adicionar mais %s: ", product.name);
+        product.addProduct(sc.nextInt());
+        
+        System.out.println(product.totalValue());
+        
+        System.out.printf("Remover alguma %s: ", product.name);
+        product.removeProduct(sc.nextInt());
+        
+        System.out.println(product.totalValue());
+
+        sc.close();
+    }
+}
