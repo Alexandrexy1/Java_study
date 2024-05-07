34ea1a6f61e6fc72c21540f516a02a1539739b69
Alexandre Nascimento da Silva <Alexandrenas2003@gmail.com>
2024-05-07 11:31:06 -0300

    try-catch installments

--- interfaces-in-java/installments/application/Program.java
+++ interfaces-in-java/installments/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -16,29 +18,37 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        System.out.println("Enter the contract data:");
-        System.out.print("Number: ");
-        int number = sc.nextInt();
-        sc.nextLine();
+        try {
+            System.out.println("Enter the contract data:");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            sc.nextLine();
 
-        System.out.print("Date (dd/MM/yyyy): ");
-        LocalDate date = LocalDate.parse(sc.nextLine(), dtf);
+            System.out.print("Date (dd/MM/yyyy): ");
+            LocalDate date = LocalDate.parse(sc.nextLine(), dtf);
 
-        System.out.print("Contract value: ");
-        double totalValue = sc.nextDouble();
+            System.out.print("Contract value: ");
+            double totalValue = sc.nextDouble();
 
-        System.out.print("Number of installments: ");
-        int months = sc.nextInt();
+            System.out.print("Number of installments: ");
+            int months = sc.nextInt();
 
-        Contract contract = new Contract(number, date, totalValue);
+            Contract contract = new Contract(number, date, totalValue);
 
-        ContractService service = new ContractService(new PaypalService());
-        service.processContract(contract, months);
+            ContractService service = new ContractService(new PaypalService());
+            service.processContract(contract, months);
 
-        System.out.println("Installments:");
-        for (Installment installment: contract.getInstallments()) {
-            System.out.println(installment);
+            System.out.println("Installments:");
+            for (Installment installment: contract.getInstallments()) {
+                System.out.println(installment);
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        } catch(DateTimeParseException e) {
+            System.out.println("Invalid date format, please use dd/MM/yyyy");
+        } finally {
+            sc.close();
         }
-        sc.close();
     }
 }
